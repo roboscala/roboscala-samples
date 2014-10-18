c33da3a3a72ecf87eb413656578fb62931ded4eb
Joseph Hager <ajhager@gmail.com>
2014-10-18 15:52:13 -0400

    Remove unused dependencies

--- project/build.scala
+++ project/build.scala
@@ -9,12 +9,9 @@ object ScaliOSBuild extends Build {
   lazy val empty = makeDemo("empty", "Empty Robo")
 
   val roboVersion = "1.0.0-alpha-04"
-  val roboDependencies = Seq("org.robovm" % "robovm-compiler" % roboVersion
-                          ,"org.robovm" % "robovm-rt" % roboVersion
-                          ,"org.robovm" % "robovm-objc" % roboVersion
-                          , "org.robovm" % "robovm-cocoatouch" % roboVersion
-                          , "org.robovm" % "robovm-cacerts-full" % roboVersion
-)
+  val roboDependencies = Seq("robovm-rt", "robovm-cocoatouch")
+    .map("org.robovm" % _ % roboVersion)
+
   def makeDemo(path: String, name: String, settings: Seq[Setting[_]] = Seq.empty): Project = {
     RobovmProject(path, file(path),
       settings = Defaults.defaultSettings ++ settings ++ Seq(
