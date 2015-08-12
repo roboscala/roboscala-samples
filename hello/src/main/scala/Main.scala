97d5bae6d5196c9bbe54f738b85d527f0ca9fb76
outofcoffee <outofcoffee@gmail.com>
2015-08-12 22:11:26 +0100

    Update Main.scala

--- hello/src/main/scala/Main.scala
+++ hello/src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import org.robovm.apple.uikit._
 class HelloWorld extends UIApplicationDelegateAdapter {
   lazy val window = new UIWindow(UIScreen.getMainScreen.getBounds)
 
-  override def didFinishLaunching(applicaiton: UIApplication, launchOptions: UIApplicationLaunchOptions): Boolean = {
+  override def didFinishLaunching(application: UIApplication, launchOptions: UIApplicationLaunchOptions): Boolean = {
     window.setRootViewController(new MyViewController())
     window.makeKeyAndVisible()
     addStrongRef(window)
